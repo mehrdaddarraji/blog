1946e84b5aecbf862952d74a3531807b89d14021
Mehrdad Darraji <mehrdad_da@yahoo.com>
2020-08-13 01:04:18 -0500

    user entity

---
+++ src/main/java/com/mehrdaddarraji/techblog/entity/User.java
@@ -0,0 +1,64 @@
+package com.mehrdaddarraji.techblog.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	@Column(name = "FIRST_NAME")
+    private String firstName;
+	@Column(name = "LAST_NAME")
+    private String lastName;
+	@Column(name = "EMAIL")
+    private String email;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "ROLE")
+    private String role;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+}
